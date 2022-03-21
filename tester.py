1642fa97290b13c85af8e6f5952461bda44db497
OE7DIO <stefan.wegleiter@gmail.com>
2022-03-21 16:16:44 +0100

    created a tester

--- tester.py
+++ tester.py
@@ -1,4 +1,12 @@
-from pc_test_version import vor, linksUm, vornFrei
+from pc_test_version import vor, linksUm, ausschalten, setIP
 
+setIP("27.0.1.0")
 
+vor()
+vor()
 
+setIP("127.0.0.1")
+
+vor()
+
+print("juh")
